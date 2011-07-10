70918977405b91030b37b0fd39699e40c52b473e
Matt Bertolini <viper2843@gmail.com>
2011-07-09 23:19:17 -0400

    Adding some files for testing. Adding the ability to use both the short and long versions of the key id. Adding ability to change the output directory of the signature files.

--- src/java/com/mattbertolini/antpgp/PgpSignTask.java
+++ src/java/com/mattbertolini/antpgp/PgpSignTask.java
@@ -26,6 +26,7 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openpgp.PGPException;
 import org.bouncycastle.openpgp.PGPPrivateKey;
 import org.bouncycastle.openpgp.PGPSecretKey;
+import org.bouncycastle.openpgp.PGPSecretKeyRing;
 import org.bouncycastle.openpgp.PGPSecretKeyRingCollection;
 import org.bouncycastle.openpgp.PGPSignature;
 import org.bouncycastle.openpgp.PGPSignatureGenerator;
@@ -38,12 +39,15 @@ import org.bouncycastle.openpgp.PGPUtil;
  */
 public class PgpSignTask extends Task {
     private static final int RADIX_BASE_16 = 16;
+    private static final long KEY_ID_MASK = 0xFFFFFFFFL;
+    private static final String FILE_SEPARATOR = System.getProperty("file.separator");
 
     private Provider securityProvider;
     private File secretKeyring;
     private String keyId;
     private String passphrase;
     private boolean armor;
+    private File outputDir;
     private Union files;
 
     @Override
@@ -62,12 +66,16 @@ public class PgpSignTask extends Task {
         } else if (this.passphrase == null || this.passphrase.isEmpty()) {
             throw new BuildException("Passphrase is null or empty.");
         }
-        InputStream keyringInputStream = null;
+        
+        if(!this.outputDir.isDirectory()) {
+            throw new BuildException("Output dir is not a directory.");
+        }
+        
         try {
-            long keyIdAsLong = Long.parseLong(this.keyId, RADIX_BASE_16);
-            keyringInputStream = PGPUtil.getDecoderStream(new FileInputStream(this.secretKeyring));
-            PGPSecretKeyRingCollection secretKeyrings = new PGPSecretKeyRingCollection(keyringInputStream);
-            PGPSecretKey secretKey = secretKeyrings.getSecretKey(keyIdAsLong);
+            PGPSecretKey secretKey = this.getSecretKey();
+            if(secretKey == null) {
+                throw new BuildException("Could not find secret key with key ID " + this.keyId + " in keyring " + this.secretKeyring);
+            }
             PGPPrivateKey privateKey = secretKey.extractPrivateKey(this.passphrase.toCharArray(), this.securityProvider);
             PGPSignatureGenerator sigGenerator = new PGPSignatureGenerator(secretKey.getPublicKey().getAlgorithm(), PGPUtil.SHA1, this.securityProvider);
             sigGenerator.initSign(PGPSignature.BINARY_DOCUMENT, privateKey);
@@ -89,16 +97,20 @@ public class PgpSignTask extends Task {
                         throw new BuildException(filePath + " not found.");
                     }
                     log("Signing " + filePath);
-                    fileToSignInputStream = new BufferedInputStream(
-                            new FileInputStream(file));
+                    fileToSignInputStream = new BufferedInputStream(new FileInputStream(file));
                     int valueByte;
                     while ((valueByte = fileToSignInputStream.read()) >= 0) {
                         sigGenerator.update((byte) valueByte);
                     }
+                    String outputPath = filePath;
+                    if(this.outputDir != null) {
+                        outputPath = this.outputDir.getAbsolutePath() + FILE_SEPARATOR + file.getName();
+                        log("Saving file to " + outputPath);
+                    }
                     if (this.armor) {
-                        signatureFileOutputStream = new ArmoredOutputStream(new FileOutputStream(filePath + ".asc"));
+                        signatureFileOutputStream = new ArmoredOutputStream(new FileOutputStream(outputPath + ".asc"));
                     } else {
-                        signatureFileOutputStream = new BufferedOutputStream(new FileOutputStream(filePath + ".sig"));
+                        signatureFileOutputStream = new BufferedOutputStream(new FileOutputStream(outputPath + ".sig"));
                     }
                     sigGenerator.generate().encode(signatureFileOutputStream);
                 } catch (FileNotFoundException e) {
@@ -114,17 +126,42 @@ public class PgpSignTask extends Task {
                     FileUtils.close(signatureFileOutputStream);
                 }
             }
+        } catch (PGPException e) {
+            throw new BuildException(e);
+        } catch (NoSuchAlgorithmException e) {
+            throw new BuildException(e);
+        }
+    }
+    
+    @SuppressWarnings("unchecked")
+    private PGPSecretKey getSecretKey() throws BuildException {
+        InputStream keyringInputStream = null;
+        PGPSecretKey secretKey = null;
+        try {
+            long keyIdAsLong = Long.parseLong(this.keyId, RADIX_BASE_16);
+            keyringInputStream = PGPUtil.getDecoderStream(new FileInputStream(this.secretKeyring));
+            PGPSecretKeyRingCollection secretKeyrings = new PGPSecretKeyRingCollection(keyringInputStream);
+            for (Iterator<PGPSecretKeyRing> secretKeyringIter = secretKeyrings.getKeyRings(); secretKey == null && secretKeyringIter.hasNext();) {
+                PGPSecretKeyRing secretKeyring = secretKeyringIter.next();
+                for (Iterator<PGPSecretKey> secretKeyIter = secretKeyring.getSecretKeys(); secretKey == null && secretKeyIter.hasNext();) {
+                    PGPSecretKey key = secretKeyIter.next();
+                    if(keyIdAsLong == key.getKeyID()) {
+                        secretKey = key;
+                    } else if(keyIdAsLong == (key.getKeyID() & KEY_ID_MASK)) {
+                        secretKey = key;
+                    }
+                }
+            }
         } catch (FileNotFoundException e) {
             throw new BuildException(e);
         } catch (IOException e) {
             throw new BuildException(e);
         } catch (PGPException e) {
             throw new BuildException(e);
-        } catch (NoSuchAlgorithmException e) {
-            throw new BuildException(e);
         } finally {
             FileUtils.close(keyringInputStream);
         }
+        return secretKey;
     }
 
     /**
@@ -138,7 +175,7 @@ public class PgpSignTask extends Task {
     }
 
     /**
-     * Required. The full 8 byte hexadecimal PGP key ID.
+     * Required. The PGP key ID.
      * 
      * @param keyId
      * @since 1.0.0
@@ -165,6 +202,16 @@ public class PgpSignTask extends Task {
      */
     public void setArmor(boolean armor) {
         this.armor = armor;
+    }
+    
+    /**
+     * Optional. Changes the directory where the signature files will be saved. 
+     * Defaults to the same directory as the files being signed.
+     * 
+     * @param outputDir
+     */
+    public void setOutputDir(File outputDir) {
+        this.outputDir = outputDir;
     }
 
     public void add(ResourceCollection rc) {
