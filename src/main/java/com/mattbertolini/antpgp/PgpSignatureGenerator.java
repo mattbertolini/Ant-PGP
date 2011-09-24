fe2ff219e58ab90c7c96a5894ffe36e01377116c
Matt Bertolini <viper2843@gmail.com>
2011-09-23 20:59:13 -0400

    Starting to add some javadoc.

--- src/main/java/com/mattbertolini/antpgp/PgpSignatureGenerator.java
+++ src/main/java/com/mattbertolini/antpgp/PgpSignatureGenerator.java
@@ -9,6 +9,16 @@ import java.io.IOException;
  * @author Matt Bertolini
  */
 public interface PgpSignatureGenerator {
+    /**
+     * Initializes the signature generator with the given PGP secret keyring file, key ID, and passphrase. The 
+     * signature generator cannot be used until the initialization has occurred.
+     * @param secretKeyring The PGP secret keyring file containing a key matching the key id.
+     * @param keyId The key id to look for in the keyring file.
+     * @param passphrase The passphrase of the key in the keyring file.
+     * @throws FileNotFoundException If the secret keyring file is not found.
+     * @throws IOException
+     * @throws PgpSignatureGeneratorException
+     */
     void init(File secretKeyring, String keyId, String passphrase) throws FileNotFoundException, IOException, PgpSignatureGeneratorException;
     void signFile(File fileToSign, File outputFile, boolean armor) throws FileNotFoundException, IOException, PgpSignatureGeneratorException;
 }
